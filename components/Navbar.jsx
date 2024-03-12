c8e9c59f6c64d072eb458b041d17d4eee5dd100f
Ashika Sonia Dsilva <92381352+ashikadsilva@users.noreply.github.com>
2024-03-12 22:33:57 +0530

    Add files via upload

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+<div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
       <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between ">
         <Link to="/">
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.1 }}>
@@ -52,6 +52,7 @@ const Navbar = () => {
                 Contact
               </motion.li>
             </Link>
+            
           </ul>
           <a to="/assets/noor_resume.pdf" target="_blank">
             <motion.button
@@ -63,6 +64,14 @@ const Navbar = () => {
               Resume
             </motion.button>
           </a>
+        </div>
+        {/* Icon  */}
+        <div
+          className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group"
+        >
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
         </div>
       </div>
     </div>
